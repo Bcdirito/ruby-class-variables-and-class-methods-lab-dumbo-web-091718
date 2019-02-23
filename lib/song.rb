7c1aef21c8b5c042a248326d19943a84f22c598d
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 02:53:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,13 +4,6 @@ require "pry"
 
 class Song
 
- def self.new(name, artist, genre)
+ def initialize(name, artist, genre)
    @name = name
-   @artist = artist
-   @genre = genre
- end
- 
- def name
-   @name
- end
 end
