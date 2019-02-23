cd93301160aa1266ce82f6300f6391bb7c54e932
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 03:02:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
  def self.artist_count
    artists_hash = Hash.new(0)
    @@artists.each do |artist|
-     if artists_hash["#{genre}"]
+     if artists_hash["#{artist}"]
        artists_hash["#{genre}"] += 1
      else
        artists_hash["#{genre}"]
