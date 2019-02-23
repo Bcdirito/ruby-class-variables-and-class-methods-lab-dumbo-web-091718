006192d6ebc40e4ea8534bc3b803d31f5aeec1ad
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 02:55:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,11 +8,13 @@ class Song
   @@artists = []
   @@genres = []
   @@count = 0
+  
  def initialize(name, artist, genre)
    @name = name
    @artist = artist
    @genre = genre
    @@artists << self.artist
    @@genres << self.genre
+   @@count += 1
  end
 end
